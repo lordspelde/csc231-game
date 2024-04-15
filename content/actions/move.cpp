a7bd42a339ff4c5dd2c1fa34234ec0043d757551
lordspelde <rwspelde15@gmail.com>
2024-04-14 20:12:50 -0500

    Implemented Move::perform()

--- content/actions/move.cpp
+++ content/actions/move.cpp
@@ -1,5 +1,18 @@
+#include "entity.h"
+#include "tile.h"
+#include "engine.h"
 #include "move.h"
 
 Move::Move(Vec direction):direction {direction} {}
 
-Result Move::perform(Engine& engine, std::shared_ptr<Entity> entity) {}
+Result Move::perform(Engine& engine, std::shared_ptr<Entity> entity) {
+    entity->change_direction(direction);
+    Vec goto_dir = entity->get_position() + direction;
+
+    if (engine.dungeon.get_tile(goto_dir).walkable) {
+        entity->move_to(goto_dir);
+        return success();
+    }
+
+    return failure();
+}
