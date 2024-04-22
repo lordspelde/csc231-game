95cff0718f7c2a080b6acb87a89bcd25c0f87a04
lordspelde <rwspelde15@gmail.com>
2024-04-22 00:16:38 -0500

    Added monsters

--- main.cpp
+++ main.cpp
@@ -11,6 +11,11 @@ int main() {
         std::shared_ptr<Entity> hero = engine.create_hero();
         Heroes::make_wizard(hero);
 
+        for (int i = 0; i < 20; i++) {
+            std::shared_ptr<Entity> monster = engine.create_monster();
+            Monsters::make_monster(monster);
+        }
+
         engine.run();
     }
     catch (std::exception& e) {
