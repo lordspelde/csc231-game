22379808936d4d00e6d58c8a670256ca9cde74b1
lordspelde <rwspelde15@gmail.com>
2024-04-29 13:38:54 -0500

    Bite attack now does damage

--- content/items/bite.cpp
+++ content/items/bite.cpp
@@ -1,9 +1,11 @@
 #include "bite.h"
 #include "engine.h"
 
+#include "hit.h"
+
 Bite::Bite(int damage)
-    :Item{"none"} {}
+    :Item {"none"}, damage {damage} {}
 
 void Bite::use(Engine& engine, Entity&, Entity& defender) {
-
+    engine.events.create_event<Hit>(defender, damage);
 }
