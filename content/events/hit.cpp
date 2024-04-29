4d57633c98e5ffd3a101c58c087a489168674ca0
lordspelde <rwspelde15@gmail.com>
2024-04-29 13:39:37 -0500

    Removed engine variable name from function headers

--- content/events/hit.cpp
+++ content/events/hit.cpp
@@ -5,11 +5,11 @@
 Hit::Hit(Entity& entity, int damage)
     :entity {entity}, damage {damage} {}
 
-void Hit::execute(Engine& engine) {
+void Hit::execute(Engine&) {
     entity.take_damage(damage);
 }
 
-void Hit::when_done(Engine& engine) {
+void Hit::when_done(Engine&) {
     if (!entity.is_alive()) {
         add_next(Die {entity});
     }
