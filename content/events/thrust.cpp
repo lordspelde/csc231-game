2df9c7a3111d45e1a49fdf68442779fb403de079
lordspelde <rwspelde15@gmail.com>
2024-05-07 21:11:13 -0500

    Added thrust animation

---
+++ content/events/thrust.cpp
@@ -0,0 +1,35 @@
+#include "thrust.h"
+
+constexpr int duration = 3;  // # of frames
+constexpr int delta = 3;     // pixels per frame
+
+Thrust::Thrust(Sprite& sprite, Vec direction)
+    : Event{duration}, sprite{sprite}, copy{sprite}, direction(direction) {
+    // orient the weapon towards
+    sprite.center = sprite.size / 2;
+    sprite.shift.y -= sprite.size.y / 4;
+
+    if (direction == Vec{1, 0}) {  // RIGHT
+        sprite.shift.y += sprite.size.y / 2;
+        sprite.angle = 90;
+    } else if (direction == Vec{-1, 0}) {  // LEFT
+        sprite.shift.y += sprite.size.y / 2;
+        sprite.angle = -90;
+    } else if (direction == Vec{0, 1}) {  // UP
+        sprite.angle = 0;
+        this->direction = -1 * direction;
+    } else {  // DOWN
+        sprite.angle = 180;
+        sprite.shift.y += sprite.size.y;
+        this->direction = -1 * direction;
+    }
+}
+
+void Thrust::execute(Engine&) {
+    // shift in pixels (16 per tile)
+    sprite.shift += direction * delta;
+}
+
+void Thrust::when_done(Engine&) {
+    sprite = copy;
+}
